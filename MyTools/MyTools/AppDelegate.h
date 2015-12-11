6ef44a7eb9fdc0fdd4b21cdedba9cdf856e2e9d8
lixuanlin <lixuanlin@txcap.com>
2015-12-11 14:55:37 +0800

    第一次试验

---
+++ MyTools/MyTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTools
+//
+//  Created by 李轩霖 on 15/12/11.
+//  Copyright © 2015年 李轩霖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTools/MyTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTools
+//
+//  Created by 李轩霖 on 15/12/11.
+//  Copyright © 2015年 李轩霖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
